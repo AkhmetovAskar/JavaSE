6540ebd3a5bf9222fe0827d44ad717b0e976c36a
AkhmetovAskar <akhmetoff@bk.ru>
2017-07-29 16:16:48 +0500

    Загрузка проекта на GitHub

---
+++ src/main/java/tutorial/generics/PairOfTheDiffType.java
@@ -0,0 +1,35 @@
+package tutorial.generics;
+
+/**
+ * Created by askar on 04.06.2017.
+ * Пример обобщенного класса, у которого несколько типов-параметров T, U.
+ * Конечно же обобщенный класс может содержать и больше типов-параметров
+ */
+public class PairOfTheDiffType<T, U> {
+    private T first;
+    private U second;
+
+    public PairOfTheDiffType() {
+    }
+
+    public PairOfTheDiffType(T first, U second) {
+        this.first = first;
+        this.second = second;
+    }
+
+    public T getFirst() {
+        return first;
+    }
+
+    public void setFirst(T first) {
+        this.first = first;
+    }
+
+    public U getSecond() {
+        return second;
+    }
+
+    public void setSecond(U second) {
+        this.second = second;
+    }
+}
---
+++ src/main/java/tutorial/generics/Wildcards.java
@@ -0,0 +1,33 @@
+package tutorial.generics;
+
+/**
+ * Created by askar on 12.06.2017.
+ * Wildcard - подстановочный символ ?, означает "любой тип"
+ */
+public class Wildcards {
+
+    //обычно ? используется в простых случаях, когда тип не важен
+    public boolean hasNulls(PairOfTheSameType<?> pair) {
+        return pair.getFirst() == null || pair.getSecond() == null;
+    }
+
+    //? можно ограничить через extends (? extends Type).
+    //Это означает, что тип может быть подтипом Type или Type
+    public void printNumbers(PairOfTheSameType<? extends Number> pair) {
+        System.out.println("First : " + pair.getFirst().toString());
+        System.out.println("Second : " + pair.getSecond().toString());
+    }
+
+    //аналогично методу выше, только с использованием тип-параметра
+    public <T extends Number> void printNumbers2(PairOfTheSameType<T> pair) {
+        System.out.println("First : " + pair.getFirst().toString());
+        System.out.println("Second : " + pair.getSecond().toString());
+    }
+
+    //? можно ограничить через super (? super Type).
+    //Это означает, что тип может быть любым супертипом Type
+    public void test(PairOfTheSameType<? super Integer> pair) {
+
+    }
+
+}
