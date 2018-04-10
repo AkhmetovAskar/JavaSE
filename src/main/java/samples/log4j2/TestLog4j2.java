c95161863b796adfdbfa4b07aba8ca3ee6f83523
aakhmetov <aakhmetov@it.ru>
2018-04-10 13:21:04 +0500

    Log4j2

---
+++ src/main/java/samples/log4j2/TestLog4j2.java
@@ -0,0 +1,19 @@
+package samples.log4j2;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class TestLog4j2 {
+    private static final Logger logger = LogManager.getLogger(TestLog4j2.class);
+
+    public static void main(String[] args) {
+
+        logger.debug("This Will Be Printed On Debug");
+        logger.info("This Will Be Printed On Info");
+        logger.warn("This Will Be Printed On Warn");
+        logger.error("This Will Be Printed On Error");
+        logger.fatal("This Will Be Printed On Fatal");
+
+        logger.info("Appending string: {}", "Hello, World");
+    }
+}
